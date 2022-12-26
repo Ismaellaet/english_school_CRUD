f07ff4ffb3d4ad4d9f6c1add7c0485e7b22998d1
Ismael Laet <ismaelabashi@hotmail.com>
2022-12-26 16:46:24 -0300

    feat: Enrollments CRUD

--- src/controllers/PersonController.js
+++ src/controllers/PersonController.js
@@ -1,7 +1,7 @@
 const db = require("../app/models");
 
 class PersonController {
-	static async list(req, res) {
+	static async listPeople(req, res) {
 		try {
 			const people = await db.Person.findAll();
 			return res.json(people);
@@ -10,7 +10,7 @@ class PersonController {
 		}
 	}
 
-	static async read(req, res) {
+	static async readPerson(req, res) {
 		const { id } = req.params;
 
 		try {
@@ -26,7 +26,7 @@ class PersonController {
 		}
 	}
 
-	static async create(req, res) {
+	static async createPerson(req, res) {
 		try {
 			const person = await db.Person.create(req.body);
 			return res.status(201).json(person);
@@ -35,7 +35,7 @@ class PersonController {
 		}
 	}
 
-	static async update(req, res) {
+	static async updatePerson(req, res) {
 		const { id } = req.params;
 		const newInfos = req.body;
 
@@ -53,11 +53,88 @@ class PersonController {
 		}
 	}
 
-	static async delete(req, res) {
+	static async deletePerson(req, res) {
 		const { id } = req.params;
 		try {
 			await db.Person.destroy({ where: { id } });
 			return res.json({ message: `Person ${id} deleted successfully!` });
+		} catch (err) {
+			return res.status(500).json(err.message);
+		}
+	}
+
+	static async readEnrollment(req, res) {
+		const { studentId, enrollmentId } = req.params;
+
+		try {
+			const enrollment = await db.Enrollment.findOne({
+				where: {
+					id: enrollmentId,
+					student_id: studentId,
+				},
+			});
+
+			if (!enrollment) {
+				return res.json({ message: `Enrollment ${enrollmentId} not found!` });
+			}
+
+			return res.json(enrollment);
+		} catch (err) {
+			return res.status(500).json(err.message);
+		}
+	}
+
+	static async createEnrollment(req, res) {
+		const { studentId } = req.params;
+		try {
+			const enrollment = await db.Enrollment.create({
+				...req.body,
+				student_id: studentId,
+			});
+			return res.status(201).json(enrollment);
+		} catch (err) {
+			return res.status(500).json(err.message);
+		}
+	}
+
+	static async updateEnrollment(req, res) {
+		const { studentId, enrollmentId } = req.params;
+		const newInfos = { ...req.body };
+
+		try {
+			await db.Enrollment.update(newInfos, {
+				where: {
+					id: enrollmentId,
+					student_id: studentId,
+				},
+			});
+			const enrollment = await db.Enrollment.findOne({
+				where: {
+					id: enrollmentId,
+					student_id: studentId,
+				},
+			});
+
+			if (!enrollment) {
+				return res.json({ message: `Enrollment ${enrollmentId} not found!` });
+			}
+
+			return res.json(enrollment);
+		} catch (err) {
+			return res.status(500).json(err.message);
+		}
+	}
+
+	static async deleteEnrollment(req, res) {
+		const { studentId, enrollmentId } = req.params;
+		try {
+			await db.Enrollment.destroy({
+				where: {
+					id: enrollmentId,
+					student_id: studentId,
+				},
+			});
+			return res.json({ message: `Enrollment ${enrollmentId} deleted successfully!` });
 		} catch (err) {
 			return res.status(500).json(err.message);
 		}
--- src/routes/peopleRoutes.js
+++ src/routes/peopleRoutes.js
@@ -4,10 +4,14 @@ const PersonController = require("../controllers/PersonController");
 const router = Router();
 
 router
-	.get("/people", PersonController.list)
-	.get("/people/:id", PersonController.read)
-	.post("/people", PersonController.create)
-	.put("/people/:id", PersonController.update)
-	.delete("/people/:id", PersonController.delete);
+	.get("/people", PersonController.listPeople)
+	.get("/people/:id", PersonController.readPerson)
+	.post("/people", PersonController.createPerson)
+	.put("/people/:id", PersonController.updatePerson)
+	.delete("/people/:id", PersonController.deletePerson)
+	.get("/people/:studentId/enrollments/:enrollmentId", PersonController.readEnrollment)
+	.post("/people/:studentId/enrollments/", PersonController.createEnrollment)
+	.put("/people/:studentId/enrollments/:enrollmentId", PersonController.updateEnrollment)
+	.delete("/people/:studentId/enrollments/:enrollmentId", PersonController.deleteEnrollment)
 
 module.exports = router;
