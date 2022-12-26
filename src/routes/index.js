e4e7b2c9d7528bfdcf00275424440b8a013ad9ed
Ismael Laet <ismaelabashi@hotmail.com>
2022-12-26 16:46:43 -0300

    feat: Add new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const peopleRoutes = require("./peopleRoutes");
+const levelsRoutes = require("./levelsRoutes");
+const classesRoutes = require("./classesRoutes");
+
 
 module.exports = app => {
-	app.use(express.json(), peopleRoutes);
+	app.use(express.json(), peopleRoutes, levelsRoutes, classesRoutes);
 };
