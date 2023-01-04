e5dc179ae92c6f5451028174fcc885dec056753d
Ismael Laet <ismaelabashi@hotmail.com>
2023-01-04 17:49:33 -0300

    Fix table name in seed file

--- src/database/seeders/20221223100255-demo-class.js
+++ src/database/seeders/20221223100255-demo-class.js
@@ -40,6 +40,6 @@ module.exports = {
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete("Turmas", null, {});
+		await queryInterface.bulkDelete("Classes", null, {});
 	},
 };
